c8cde10321c464ed8854551451b03857c8d110f5
Abdullah <abdullah.kindy@gmail.com>
2025-07-13 21:02:16 +0200

    feat: add more mock + add mat table

--- frontend/src/app/user-management/user-list/user-list.component.ts
+++ frontend/src/app/user-management/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../_services/user.service';
 import { User } from '../_models/user.model';
@@ -7,7 +8,7 @@ import { User } from '../_models/user.model';
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
@@ -15,6 +16,14 @@ export class UserListComponent implements OnInit {
   users: User[] = [];
   loading = true;
   error: string | null = null;
+  displayedColumns: string[] = [
+    'name',
+    'email',
+    'phone',
+    'street',
+    'zipcode',
+    'role',
+  ];
 
   constructor(private userService: UserService) {}
 
